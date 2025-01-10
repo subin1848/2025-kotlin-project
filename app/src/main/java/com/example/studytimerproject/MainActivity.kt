a78d69fde53ac6a51c4f47c7106a398cf943f706
subin1848 <s2315@e-mirim.hs.kr>
2025-01-11 00:31:05 +0900

    ✨ [refactor] DataBindingUtil 미사용 import 삭제

--- app/src/main/java/com/example/studytimerproject/MainActivity.kt
+++ app/src/main/java/com/example/studytimerproject/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
