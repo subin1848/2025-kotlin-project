c76cad3297edb7348c197a534cb952cb3a471a6e
subin1848 <s2315@e-mirim.hs.kr>
2025-01-12 18:26:25 +0900

    📝 [chore] MPandroidchart 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
+    // MPandroidchart
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
